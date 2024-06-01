b8ecd953220a3dbf4ecd51eb61a6d364c2f04ada
bhavishjagani <bhavishjagani2@gmail.com>
2024-06-01 14:34:33 -0700

    Completed Knight, King, and Queen Validation

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Game {
     private static Board board;
     private boolean gameOver;
@@ -11,6 +12,14 @@ public class Game {
         } else {
             System.out.println("Invalid move");
         }
+    }
+    public static void getInput() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the starting coordinates in algebraic notation: ");
+        String startCoordinates = sc.next();
+        System.out.println("Enter the destination coordinates in algebraic notation: ");
+        String endCoordinates = sc.next();
+        System.out.println(Character.getNumericValue('c'));
     }
 
     public static void main(String[] args) {
@@ -47,5 +56,45 @@ public class Game {
         board.printBoard();
         move(2, 5, 0, 3);
         board.printBoard();
+
+        //Attempt to move a white knight from b1 to c3
+        System.out.println("Trying to move a white knight from 1, 7 to 2, 5 (b1 to c3)");
+        move(1, 7, 2, 5);
+        board.printBoard();
+        move(2, 5, 2, 4);
+        board.printBoard();
+        move(2, 5, 1, 3);
+        board.printBoard();
+        move(1, 3, 2, 1);
+        board.printBoard();
+        move(2, 1, 0, 2);
+        board.printBoard();
+        move(0, 2, 2, 3);
+        board.printBoard();
+        move(2, 3, 0, 4);
+        board.printBoard();
+
+        //Attempt to move a white queen from d1 to a1
+        System.out.println("Trying to move a white queen from 3, 7 to 0, 7 (d1 to a1)");
+        move(3, 7, 0, 7);
+        board.printBoard();
+        move(0, 7, 3, 4);
+        board.printBoard();
+        move(3, 4, 3, 1);
+        board.printBoard();
+        move(3, 1, 0, 4);
+        board.printBoard();
+        move(4, 6, 4, 4);
+        board.printBoard();
+        move(4, 7, 4, 6);
+        board.printBoard();
+        move(4, 6, 3, 5);
+        board.printBoard();
+        move(3, 5, 4, 4);
+        board.printBoard();
+
+        System.out.println((int) 'a');
+        //Algebraic Notation
+
     }
 }
